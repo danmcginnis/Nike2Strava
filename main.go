72f7e91a8da7821b7b41c567018163bdb3f3fb0c
Dan M McGinnis <mcginnis.dan@gmail.com>
2015-12-09 19:39:04 -0500

    Echoing username and password back to the terminal

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"html/template"
+)
+
+func echoInput(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("public/index.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", echoInput)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }

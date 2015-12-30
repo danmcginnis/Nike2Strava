46bcbc2abc100d6b9d6635657848a91cfde44af5
Dan M McGinnis <mcginnis.dan@gmail.com>
2015-12-29 21:22:02 -0500

    Read Dynamically pull data based on static list of IDs

--- nikeJSON.go
+++ nikeJSON.go
@@ -3,82 +3,122 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-  "strings"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
+var ActDetails = "https://api.nike.com/v1/me/sport/activities/"
+
 type nikeDataSimple struct {
-  Data []struct {
-    ActivityId string
-    ActivityType string
-    StartTime string
-    Status string
-    MetricSummary struct {
-      Distance string
-      Duration string
-    }
-  }
-  Paging struct {
-    Next string
-    Previous string
-  }
+	Data []struct {
+		ActivityId    string
+		ActivityType  string
+		StartTime     string
+		Status        string
+		MetricSummary struct {
+			Distance string
+			Duration string
+		}
+	}
+	Paging struct {
+		Next     string
+		Previous string
+	}
 }
 
 type nikeDataComplete struct {
-  Data []struct {
-    Links []struct {
-      Rel string
-      Href string
-    }
-    ActivityId string
-    ActivityType string
-    StartTime string
-    ActivityTimeZone string
-    Status string
-    DeviceType string
-    MetricSummary struct {
-      Calories string
-      Distance string
-      Duration string
-    }
-  }
-  Paging struct {
-    Next string
-    Previous string
-  }
-  Tags []struct {
-    TagType string
-    TagValue string
-  }
-  Metrics []struct {
-    IntervalMetric float64
-    IntervalUnit string
-    MetricType string
-    Values []string
-  }
-  IsGPSActivity bool
-  ElevationLoss float64
-  ElevationGain float64
-  ElevationMax float64
-  ElevationMin float64
-  IntervalMetric float64
-  IntervalUnit string
-  Waypoints []struct {
-    Latitude float64
-    Longitude float64
-    Elevation float64
-  }
+	Links []struct {
+		Rel  string
+		Href string
+	}
+	ActivityId       string
+	ActivityType     string
+	StartTime        string
+	ActivityTimeZone string
+	Status           string
+	DeviceType       string
+	MetricSummary    struct {
+		Calories string
+		Distance string
+		Duration string
+	}
+	Paging struct {
+		Next     string
+		Previous string
+	}
+	Tags []struct {
+		TagType  string
+		TagValue string
+	}
+	Metrics []struct {
+		IntervalMetric float64
+		IntervalUnit   string
+		MetricType     string
+		Values         []string
+	}
+	IsGPSActivity  bool
+	ElevationLoss  float64
+	ElevationGain  float64
+	ElevationMax   float64
+	ElevationMin   float64
+	IntervalMetric float64
+	IntervalUnit   string
+	Waypoints      []struct {
+		Latitude  float64
+		Longitude float64
+		Elevation float64
+	}
 }
 
 func wrangleJSON() {
-	var nike nikeDataComplete
-  json.Unmarshal(NikeBasic1, &nike)
-  json.Unmarshal(NikeActDetails, &nike)
-  json.Unmarshal(NikeActGPS, &nike)
-  fmt.Println(nike.Data[0].ActivityId)
-  fmt.Println("Activity has GPS data:", nike.IsGPSActivity)
-  fmt.Println("GPS read interval is", nike.IntervalMetric, strings.ToLower(nike.IntervalUnit))
-  for _, m := range nike.Tags {
-    fmt.Println(strings.ToLower(m.TagType), strings.ToLower(m.TagValue))
-  }
-}
+	var nikeList nikeDataSimple
+	json.Unmarshal([]byte(NikeBasic20), &nikeList)
+	for _, m := range nikeList.Data {
+		if m.ActivityType == "RUN" {
+			var nikeActs nikeDataComplete
 
+			url := ActDetails + m.ActivityId + "?access_token=" + Token
+			res, err := http.Get(url)
+
+			if err != nil {
+				panic(err.Error())
+			}
+
+			body, err := ioutil.ReadAll(res.Body)
+
+			if err != nil {
+				panic(err.Error())
+			}
+
+			json.Unmarshal(body, &nikeActs)
+
+			if nikeActs.IsGPSActivity {
+				gpsUrl := ActDetails + m.ActivityId + "/gps?access_token=" + Token
+				res, err = http.Get(gpsUrl)
+
+				if err != nil {
+					panic(err.Error())
+				}
+				body, err = ioutil.ReadAll(res.Body)
+
+				if err != nil {
+					panic(err.Error())
+				}
+				json.Unmarshal(body, &nikeActs)
+			}
+
+			fmt.Println(nikeActs.ActivityId)
+			fmt.Println(nikeActs.MetricSummary.Distance, "k")
+			fmt.Println("Activity has GPS data:", nikeActs.IsGPSActivity)
+			fmt.Println("GPS read interval is", nikeActs.IntervalMetric, strings.ToLower(nikeActs.IntervalUnit))
+			for _, m := range nikeActs.Tags {
+				fmt.Println(strings.ToLower(m.TagType), strings.ToLower(m.TagValue))
+			}
+			if nikeActs.IsGPSActivity {
+				fmt.Println(nikeActs.Waypoints[0].Latitude, nikeActs.Waypoints[0].Longitude)
+			}
+			fmt.Println()
+		}
+	}
+}
